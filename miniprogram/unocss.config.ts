6570b30700b31a9046c0a3c83ecda511977445fa
JaneyX <1850829695@qq.com>
2022-11-03 17:25:26 +0800

    v1

---
+++ miniprogram/unocss.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "unocss";
+import presetWeapp from 'unocss-preset-weapp'
+import { transformerClass } from 'unocss-preset-weapp/transformer';
+
+const include = [/\.wxml$/]
+
+export default defineConfig(
+  {
+    include,
+    presets: [
+      presetWeapp(),
+    ],
+    
+    transformers:[
+      transformerClass({
+        include
+      })
+    ]
+  }
+)
